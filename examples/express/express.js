7c4a10dcb480eef0248ee09c6d1fd1f0933cdeee
Andrewiski <adevries@digitalexample.com>
2022-02-25 21:06:29 -0500

    AutoRenew Added and working

--- examples/express/express.js
+++ examples/express/express.js
@@ -56,38 +56,21 @@ var letsEncrypt = new LetsEncrypt(letsEncryptOptions);
 
 
 //routes.get('/.well-known/acme-challenge/*', function (req, res) {
-
 var httpRequestListener = function(req, res){
     try{
-        if (req.url.startsWith('/.well-known/acme-challenge/'))
-        {    
-            let token = req.url.substring('/.well-known/acme-challenge/'.length);
-            letsEncrypt.getToken({ token:token }).then(
-                function (challenge) {
-                    if (challenge && challenge.keyAuthorization) {  //Add Expiration Check
-                        res.setHeader('content-type', 'application/octet-stream');
-                        //res.send(challenge.keyAuthorization);
-                        
-                        res.end(challenge.keyAuthorization, 'utf8');
-                        //letsEncrypt.removeToken({ token: token })
-                        //finalizeOrder();
-                        console.log("Challenge Was Returned " + req.url + " " + challenge.keyAuthorization )
-                    } else {
-                        console.log("Challenge Not Found");
-                        //res.status(404).send('Challenge Not Found');
-                        res.statusCode = 404;
-                        res.end('Challenge Not Found', 'utf8');
-                    }
-                },
-                function (ex) {
-                    
-                    //res.status(404).send(ex);
-                    res.statusCode = 500;
-                    res.end('Challenge Not Found ' + ex.message, 'utf8');
-                }
-            );
-            
-        }else if (req.url === "/") {
+        letsEncrypt.httpRequestHandler(req,res, httpRequestLisnerNext);
+    }catch(ex){
+        console.error('httpRequestListener', ex);
+        //res.status(500).send(ex);
+        res.statusCode = 500;
+        res.end(ex.message, 'utf8');
+    }
+}
+
+var httpRequestListenerNext = function(req, res){
+    //This get called as the Next if its not a well known 
+    try{
+        if (req.url === "/") {
             //res.status(200).send('<html><head><title>LetsEncrypt Example</title></head><body>Lets Encrypt Example</body></html>');
             
             res.end('<html><head><title>LetsEncrypt Example</title></head><body>Lets Encrypt Example</body></html>', 'utf8');
@@ -99,7 +82,7 @@ var httpRequestListener = function(req, res){
             res.end('File Not Found', 'utf8');
         }
     }catch(ex){
-        console.error('httpRequestListener', ex);
+        console.error('httpRequestListenerNext', ex);
         //res.status(500).send(ex);
         res.statusCode = 500;
         res.end(ex.message, 'utf8');
@@ -171,54 +154,36 @@ var startHttpsServer = function(){
 
 var checkCertificateStatus = function(){
 
-    var needToCreateCertificates = false;
-    if(fs.existsSync( httpsServerKey) == false || fs.existsSync(httpsServerCert) == false ){
-        needToCreateCertificates = true;
-    }
-    if(needToCreateCertificates === false ){
-        try{
-           var x509Cert = letsEncrypt.loadX509CertSync({certFile:httpsServerCert , keyFile:httpsServerKey });
-           if(x509Cert.isExpired === true){
-            needToCreateCertificates = true;
-           }
-           if(x509Cert.privateKeyValid === false){
-            needToCreateCertificates = true;
-           }
-        }catch(ex){
-            needToCreateCertificates = true;
+    letsEncrypt.checkCreateRenewScheduleCertificate(
+        {
+            keyFile:  httpsServerKey,
+            certFile: httpsServerCert,
+            dnsNames: dnsNames,
+            certificateSubscriberEmail:certificateSubscriberEmail,  //used to create Lets Encrypt Account
+            autoRenew: true,
+            https_srv: https_srv,  //setSecureContext will be called on this object if we get a cert to update the current cert beign used
+            useLetsEncryptStagingUrl : true,
+            skipDryRun: true,
+            skipChallengeTest: true,  //When set to false Started getting a self signed certificate in Windows on ChallengeTest but could not find what in the challenge was using TLS and exceptions had not stack other then Node TLS Calls.
+            debug: true
+            
+            //acmeServerUrlOverride: null,
+            //retryInterval: 15000,
+            //retryPending: 10,
+            //retryPoll: 10,
+            //deauthWait: 30000
+
         }
-    }
-
-    if(needToCreateCertificates === true || fs.existsSync(httpsServerKey) === false || fs.existsSync(httpsServerCert) === false ){
-        letsEncrypt.createRenewServerCertificate(
-            {
-                keyFile:  httpsServerKey,
-                certFile: httpsServerCert,
-                dnsNames: dnsNames,
-                certificateSubscriberEmail:certificateSubscriberEmail,  //used to create Lets Encrypt Account
-                https_srv: https_srv,  //setSecureContext will be called on this object if we get a cert to update the current cert beign used
-                useLetsEncryptStagingUrl : true,
-                skipDryRun: true,
-                skipChallengeTest: true,  //When set to false Started getting a self signed certificate in Windows on ChallengeTest but could not find what in the challenge was using TLS and exceptions had not stack other then Node TLS Calls.
-                debug: true
-                
-                //acmeServerUrlOverride: null,
-                //retryInterval: 15000,
-                //retryPending: 10,
-                //retryPoll: 10,
-                //deauthWait: 30000
-
-            }
-        ).then(
-            function(result){
-                console.log('Success Create/Renew Server Certificate', result);
-                updateHttpsServer()
-            },
-            function(err){
-                console.error('Error Create/Renew Server Certificate', err);
-            }
-        )    
-    }
+    ).then(
+        function(result){
+            console.log('Success Create/Renew Server Certificate', result);
+            updateHttpsServer()
+        },
+        function(err){
+            console.error('Error Create/Renew Server Certificate', err);
+        }
+    )    
+   
 }
 
 
@@ -226,10 +191,9 @@ var checkCertificateStatus = function(){
 //This function is called on an https certificate change
 var updateHttpsServer = function () {
     if(https_srv === null){
-        startHttpsServer() 
-    }
-    if(https_srv !== null){
-        https_srv.setSecureContext(getHttpsServerOptions());
+        startHttpsServer();
+        //need to update letsEncrypt.options so it will autoupdate https.setSecureContext
+        letsEncrypt.option.http_srv = https_srv;
     }
 };
 
